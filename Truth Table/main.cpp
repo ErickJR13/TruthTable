ade40655b10126a911bc23ee6b28bad27038ef44
Erick Reyes <erickjeanreyes@Ericks-MacBook-Pro.local>
2016-12-07 11:48:18 -0500

    Initial Commit

---
+++ Truth Table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Truth Table
+//
+//  Created by Erick Jean Reyes on 12/7/16.
+//  Copyright © 2016 Erick Jean Reyes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
